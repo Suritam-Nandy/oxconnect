3e2426711ab709155ff44c123e081a95cf45422d
Suritam-Nandy <13suritsuritam@gmail.com>
2022-02-01 13:30:05 +0530

    💄

---
+++ src/components/Hero.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="max-w-6xl mx-auto lg:px-2 mb-2 mt-32">
+        <div className="mb-4 p-4">
+          <div className="mb-4 p-2 grid grid-cols-2 gap-1">
+            <div className="flex flex-col justify-center mr-1">
+              <h1 className="text-4xl font-semibold tracking-wide text-gray-600">
+                Connecting Applicants to the recruiters.
+              </h1>
+              <div className="flex flex-row justify-center mt-8">
+                <div className="mr-4 p-2">
+                  <h1 className="text-2xl font-medium text-black my-2">
+                    Applicant
+                  </h1>
+                  <p className="mb-4">
+                    We are the market-leading technical interview platform to
+                    identify and hire developers wherever they are.
+                  </p>
+                  <div className="p-2 flex justify-between items-center">
+                    <button className="bg-gray-800 text-gray-300 hover:text-white hover:border-2 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                      Create Profile
+                    </button>
+                  </div>
+                </div>
+                <div className="mr-4 p-2">
+                  <h1 className="text-2xl font-medium text-black my-2">
+                    Company
+                  </h1>
+                  <p className="mb-4">
+                    We are the market-leading technical interview platform to
+                    identify and hire developers wherever they are.
+                  </p>
+                  <div className="p-2 flex justify-between items-center">
+                    <button className="  border border-gray-700 text-gray-700 bg-white hover:text-black hover:border-2 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                      Sign up
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-center  h-full">
+              <div className="h-80">
+                <img src="/Images/4565.jpg"></img>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </>
+  );
+};
+
+export default Hero;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+
 const Landing = () => {
   return (
     <>
       <Navbar />
-      <div className="mt-20">
+      <Hero />
+      {/* <div className="mt-20">
         <div className="flex flex-row justify-around">
           <div>
             <h1 className="text-4xl px-4     py-2">0xconnect</h1>
@@ -20,9 +23,9 @@ const Landing = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
 
-      <div className="max-w-6xl mx-auto lg:px-2 mb-2">
+      {/* <div className="max-w-6xl mx-auto lg:px-2 mb-2">
         <div className="mb-4 p-4">
           <div className="mb-4 p-2 grid grid-cols-2 gap-1">
             <div className="flex flex-col justify-center">
@@ -38,7 +41,7 @@ const Landing = () => {
             <div>2</div>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
