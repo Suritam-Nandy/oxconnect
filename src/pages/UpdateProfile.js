a184d0c8688f4edd136a10e0d28a11571f6ae424
Suritam-Nandy <13suritsuritam@gmail.com>
2022-03-03 14:34:58 +0530

    💄

--- src/pages/UpdateProfile.js
+++ src/pages/UpdateProfile.js
@@ -15,33 +15,41 @@ import Resume from "./Resume";
 const UpdateProfile = () => {
   const [resu, setResu] = useState(false);
   const [pro, setPro] = useState(true);
+  const [status, setStatus] = useState("Profile");
 
-  const onInputChange = () => {
-    if (!resu) setResu(!resu);
-    if (!pro) setPro(!pro);
+  const onInputChange = (status) => {
+    setStatus(status);
   };
   console.log(resu);
   return (
     <>
-      <div className="max-w-5xl flex flex-col justify-center items-center mx-auto lg:px-2 mb-2 ">
+      <div className="max-w-5xl flex flex-col justify-center items-center mx-auto lg:px-2 mb-2 cursor-default">
         <div className="w-full flex justify-start items-center my-10  ">
           <div className="w-full px-2 flex flex-col ">
             <h1 className="text-4xl my-6">Edit your profile</h1>
             <div className="my-8 flex flex-row justify-start items-center border-b border-gray-200">
               <div className=" px-3 py-2 mx-4 text-xl text-gray-600 font-medium hover:text-black w-20">
-                <h1 onClick={() => onInputChange()} className="">
+                <h1
+                  onClick={() => onInputChange("Profile")}
+                  className="cursor-pointer"
+                >
                   Profile
                 </h1>
               </div>
               <div className=" px-3 py-2 mx-4 text-xl text-gray-600 font-medium hover:text-black w-20">
-                <h1 onClick={() => onInputChange()}>Resume</h1>
+                <h1
+                  onClick={() => onInputChange("Resume")}
+                  className="cursor-pointer"
+                >
+                  Resume
+                </h1>
               </div>
             </div>
           </div>
         </div>
-        {resu ? (
+        {status === "Resume" ? (
           <Resume />
-        ) : (
+        ) : status === "Profile" ? (
           <div className="p-2  w-full h-full border border-gray-300 rounded-md cursor-default">
             <ProfileAbout />
             <SocialProfile />
@@ -51,6 +59,8 @@ const UpdateProfile = () => {
             <ProfileAchievements />
             <ProfileIdentity />
           </div>
+        ) : (
+          <></>
         )}
       </div>
     </>
